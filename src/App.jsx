a60c2006671a51fb636973b50b4c4b998277f91c
NIKHIL RANJAN KUMAR <88980648+niku-19@users.noreply.github.com>
2023-01-12 23:44:26 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import './App.css';
-import TodoList from './components/TodoList';
+import React from "react";
+import "./App.css";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
-    <div className='todo-app'>
-      <TodoList />
+    <div className="container">
+      <div className="todo-app">
+        <TodoList />
+      </div>
     </div>
   );
 }
